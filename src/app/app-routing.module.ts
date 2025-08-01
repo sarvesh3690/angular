835ed701c6938e48ed7fd77cf2a1e027f254f4fd
Sarvesh <sarveshpat27@gmail.com>
2025-08-01 13:00:02 +0530

    Initial commit - Angular Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { Option1Component } from './option1/option1.component';
+import { Option2Component } from './option2/option2.component';
+import { Option3Component } from './option3/option3.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'option1', component: Option1Component },
+  { path: 'option2', component: Option2Component },
+  { path: 'option3', component: Option3Component },
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Option1Component } from './option1/option1.component';
+import { Option2Component } from './option2/option2.component';
+import { Option3Component } from './option3/option3.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    Option1Component,
+    Option2Component,
+    Option3Component
   ],
   imports: [
     BrowserModule,
